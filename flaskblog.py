e58368c538494c58e776a77ddf45fd08003246f0
yamierite <darkote666@gmail.com>
2020-09-06 18:29:56 +0300

    added basic html templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author':'EugeneB',
+        'title':'Blog Post 1',
+        'content': 'First post content',
+        'date_posted':'September 5, 2020',
+    },
+    {
+        'author':'Alex2',
+        'title':'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted':'September 6, 2020',
+    }
+]
+
 @app.route("/")
+@app.route("/home")
 def hello():
-    return "Hello, World!"
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ =='__main__':
+    app.run(debug=True)
